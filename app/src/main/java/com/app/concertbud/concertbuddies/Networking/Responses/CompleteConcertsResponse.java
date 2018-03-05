a4d01730e3dc3ede3d4467643600bdbe0c17483f
Huong <huong.nguyen0395@gmail.com>
2018-03-05 18:30:54 -0500

    added Responses handler for networking

---
+++ app/src/main/java/com/app/concertbud/concertbuddies/Networking/Responses/CompleteConcertsResponse.java
@@ -0,0 +1,9 @@
+package com.app.concertbud.concertbuddies.Networking.Responses;
+
+/**
+ * Created by huongnguyen on 3/5/18.
+ */
+
+public class CompleteConcertsResponse {
+    
+}
---
+++ app/src/main/java/com/app/concertbud/concertbuddies/Networking/Responses/ConcertResponse.java
@@ -0,0 +1,8 @@
+package com.app.concertbud.concertbuddies.Networking.Responses;
+
+/**
+ * Created by huongnguyen on 3/5/18.
+ */
+
+public class ConcertResponse {
+}
