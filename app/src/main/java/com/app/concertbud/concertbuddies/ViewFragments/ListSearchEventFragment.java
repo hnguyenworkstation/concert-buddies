c9265779b5e6bac8fccfd3f32c3ef281cdaaedec
Huong <huong.nguyen0395@gmail.com>
2018-03-17 13:10:22 -0400

    update concerts list when new location is picked

--- app/src/main/java/com/app/concertbud/concertbuddies/ViewFragments/ListSearchEventFragment.java
+++ app/src/main/java/com/app/concertbud/concertbuddies/ViewFragments/ListSearchEventFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
@@ -27,24 +28,20 @@ import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class ListSearchEventFragment extends Fragment implements OnEventClickListener{
+public class ListSearchEventFragment extends Fragment implements OnEventClickListener {
     @BindView(R.id.events_recycler)
     RecyclerView mEventRecycler;
+
     private EventsAdapter eventsAdapter;
     private Unbinder unbinder;
-
-    /* HashMap of all Nearby Concerts */
-    private HashMap<String, EventsEntity> mConcertsHashMap = new HashMap<>();
     /* Array List of all Nearby Concerts */
     private ArrayList<EventsEntity> mConcertsList = new ArrayList<>();
-
     private int mPosition;
+    private String TAG = ListSearchEventFragment.class.getSimpleName();
 
     public ListSearchEventFragment() {
         // Required empty public constructor
@@ -87,6 +84,7 @@ public class ListSearchEventFragment extends Fragment implements OnEventClickLis
         super.onStart();
         EventBus.getDefault().register(this);
     }
+
     @Override
     public void onStop() {
         super.onStop();
@@ -133,9 +131,13 @@ public class ListSearchEventFragment extends Fragment implements OnEventClickLis
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void onEvent(ConcertsNearbyBus bus) {
         /* Put all events in HashMap to make sure no duplicated event is allowed */
+        HashMap<String, EventsEntity> mConcertsHashMap = new HashMap<>();
         for (int i = 0; i < bus.getConcerts().size(); i++) {
             mConcertsHashMap.put(bus.getConcerts().get(i).getName(), bus.getConcerts().get(i));
         }
+        Log.e(TAG, "Updating new concerts");
+        // TODO: ?? Double check if we need to clear mConcertsList
+        mConcertsList.clear();
         mConcertsList.addAll(mConcertsHashMap.values());
         eventsAdapter.notifyItemChanged(mPosition);
         EventBus.getDefault().removeStickyEvent(bus);
