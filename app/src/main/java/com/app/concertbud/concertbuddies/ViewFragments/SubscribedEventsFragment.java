73e861622d2ba8917a01996c6f02d01ea2d4275c
Hung Nguyen <hnguyenworkstation@gmail.com>
2018-04-10 11:11:36 -0400

    added view structure for subscribed events fragment

--- app/src/main/java/com/app/concertbud/concertbuddies/ViewFragments/SubscribedEventsFragment.java
+++ app/src/main/java/com/app/concertbud/concertbuddies/ViewFragments/SubscribedEventsFragment.java
@@ -2,15 +2,30 @@ package com.app.concertbud.concertbuddies.ViewFragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.app.concertbud.concertbuddies.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 
 public class SubscribedEventsFragment extends Fragment {
+    @BindView(R.id.events_recycler)
+    RecyclerView mEventsRecycler;
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private Unbinder unbinder;
+
     public SubscribedEventsFragment() {
         // Required empty public constructor
     }
@@ -36,4 +51,15 @@ public class SubscribedEventsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_subscribed_events, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
