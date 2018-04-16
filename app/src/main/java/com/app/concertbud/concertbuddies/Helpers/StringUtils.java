aae0dc86f8a18cb3183b035dbd669433889bade0
Huong <huong.nguyen0395@gmail.com>
2018-04-16 15:23:14 -0400

    remove permission for about_me and birthday for facebook

--- app/src/main/java/com/app/concertbud/concertbuddies/Helpers/StringUtils.java
+++ app/src/main/java/com/app/concertbud/concertbuddies/Helpers/StringUtils.java
@@ -9,11 +9,7 @@ public class StringUtils {
     private static final String USER_POSTS = "user_posts";
     private static final String PROFILE = "public_profile";
 
-    // <<<
-    private static final String BIRTHDAY = "user_birthday";
-    private static final String ABOUT = "user_about_me";
-
-    private static String[] FB_PERMISSIONS = {EMAIL, PROFILE, BIRTHDAY, ABOUT};
+    private static String[] FB_PERMISSIONS = {EMAIL, PROFILE};
 
     public static String[] getFbPermissions() {
         return FB_PERMISSIONS;
